0c870e4316ec4573d172349899eef6c07746766c
Stacey-33 <folorunshooluwadamilola114@gmail.com>
2023-02-19 12:46:57 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that prints single digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while (c < 10)
+	{
+		putchar(c + '0');
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
