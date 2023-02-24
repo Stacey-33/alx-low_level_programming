6ef2d1a0bf97198fb8cc19125d029d0608c5c386
Stacey-33 <folorunshooluwadamilola114@gmail.com>
2023-02-24 10:01:53 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - A program that tests for positive or negative
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
