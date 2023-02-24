3e18cf4d735bbe8c01cc059af4af3ce8c20d2fe4
Stacey-33 <folorunshooluwadamilola114@gmail.com>
2023-02-24 02:45:16 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - A function that prints the alphabets in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
